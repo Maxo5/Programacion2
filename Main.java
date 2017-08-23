1d6d32cc037dcf508631f9c7698b9c5a485d2d7e
Maxo5 <maxomenos_5@hotmail.com>
2017-08-23 20:50:30 -0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -2,16 +2,14 @@
 public class Main {
 
 	public static void main(String[] args) {
-		Pato lucas = new Pato();
-		Pato donald = new Pato("Lucas");
-		Pinguino happy = new Pinguino ();
-		happy.canta();
-		lucas.canta();
-		donald.canta();
-		lucas.vola();
-		lucas.vola();
-		happy.nada();
-		lucas.nada();
+		Calculadora calc = new Calculadora();
+		calc.reset();
+		calc.sumar(2,3);
+		calc.multiplicar(2);
+		System.out.println(calc.estado);
+
+		
+		
 	}
 
 }
